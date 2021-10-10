7e3eb84ace04dfa9e15da36d54ef1c53b908c2f6
RyanMichael <rmbh4211995@gmail.com>
2021-10-10 14:42:04 -0400

    Add log to seed-aws (working)

--- bin/cmds/db.ts
+++ bin/cmds/db.ts
@@ -2,6 +2,7 @@ import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
 import { Database } from '@leafac/sqlite'
 import { execSync } from 'child_process'
 import fs from 'fs'
+import PQueue from 'p-queue'
 import path from 'path'
 import { prisma } from '../../prisma/queries/index'
 
@@ -47,6 +48,7 @@ export const seedDb = async ({ readDir }: { readDir: string }) => {
 
 export const seedAws = async () => {
   const glbDir = path.join(__dirname, '../../../../league_react_models')
+  const queue = new PQueue({ concurrency: 30 })
   const s3 = new S3Client({
     region: 'us-east-1',
     credentials: {
@@ -55,40 +57,33 @@ export const seedAws = async () => {
     },
   })
 
-  const data = fs.readFileSync(`${glbDir}/aatrox/skin0.glb`)
-  const command = new PutObjectCommand({
-    Bucket: 'league-glb-models',
-    Body: data,
-    Key: 'aatrox/skin0.glb',
-  })
-  const response = await s3.send(command)
+  try {
+    const champDirs = fs.readdirSync(glbDir)
 
-  console.log(response)
-  // try {
-  //   const champDirs = fs.readdirSync(glbDir)
+    for (const champDir of champDirs) {
+      const files = fs.readdirSync(`${glbDir}/${champDir}`)
 
-  //   for (const champDir of champDirs) {
-  //     const files = fs.readdirSync(`${glbDir}/${champDir}`)
+      for (const file of files) {
+        queue.add(async () => {
+          const data = fs.readFileSync(`${glbDir}/${champDir}/${file}`)
+          console.log(`reading file ${glbDir}/${champDir}/${file}`)
 
-  //     for (const file of files) {
-  //       // queue.add(async () => {
-  //       //   await new Promise<void>((resolve) => {
-  //       //     exec(`npx gltfjsx ${glbDir}/${champDir}/${file} -t`, async (err, stdout, stderr) => {
-  //       //       console.log(`gltfjsx ${glbDir}/${champDir}/${file} -t > ${jsxFile}`)
-  //       //       console.log(stdout)
-  //       //       exec(
-  //       //         `mv ${jsxFile} client/src/components/${champDir}/${jsxFile}`,
-  //       //         async (err, stdout, stderr) => {
-  //       //           console.log(`mv ${jsxFile} client/src/components/${champDir}/${jsxFile} `)
-  //       //           resolve()
-  //       //         },
-  //       //       )
-  //       //     })
-  //       //   })
-  //       // })
-  //     }
-  //   }
-  // } catch (err) {
-  //   throw new Error(`Couldn't complete upload to aws s3`)
-  // }
+          const command = new PutObjectCommand({
+            Bucket: 'league-glb-models',
+            Body: data,
+            Key: `${champDir}/${file}`,
+          })
+
+          const response = await s3.send(command)
+          console.log(`uploaded ${champDir}/${file} to S3`, response)
+        })
+      }
+
+      if (queue.size >= 100) await new Promise((resolve) => setTimeout(resolve, 100))
+    }
+
+    await queue.onIdle()
+  } catch (err) {
+    throw new Error(`Couldn't complete upload to aws s3`)
+  }
 }
