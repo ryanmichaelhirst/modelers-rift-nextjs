1a3f52d64cc21647fdd9eff8a6f6f5bd5ddd7ae4
RyanMichael Hirst <ryanmichael@indigov.us>
2021-10-11 03:14:59 -0400

    Remove webpack and babel. Clean up dependencies.

--- server/index.ts
+++ server/index.ts
@@ -1,27 +1,12 @@
 import express from 'express'
-import path from 'path'
-import webpack from 'webpack'
-import webpackDevMiddleware from 'webpack-dev-middleware'
-import webpackHotMiddleware from 'webpack-hot-middleware'
 import { apolloServer } from '../graphql'
 import { getUsers } from '../prisma/queries'
 
 export default (async () => {
-  const config = require(path.join(__dirname, '../webpack.config.ts'))
-  const compiler = webpack(config)
   const app = express()
 
   await apolloServer.start()
   apolloServer.applyMiddleware({ app })
-
-  app.use(webpackDevMiddleware(compiler, { publicPath: config.output.publicPath }))
-  app.use(
-    webpackHotMiddleware(compiler, {
-      log: console.log,
-      path: '/__webpack_hmr',
-      heartbeat: 10 * 1000,
-    }),
-  )
 
   app.get('/api/getUsers', async (req, res) => {
     console.log('proxy request made to /api/getUser')
