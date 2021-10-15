293a37dcaf385fd895febb311e534fce365664b3
RyanMichael <rmbh4211995@gmail.com>
2021-10-15 04:15:43 -0400

    Convert binary string into blob. Attempt to dynamic import blob i.e. champ glb. (Not Working)

--- client/src/components/aatrox/index.tsx
+++ client/src/components/aatrox/index.tsx
@@ -4,36 +4,44 @@ import Skin2 from '@components/aatrox/skin2'
 import { useEffect, useState } from 'react'
 
 const Aatrox = ({ skin }: { skin: string }) => {
-  const [glb, setGlb] = useState()
-  const [obj, setObj] = useState()
+  const [blob, setBlob] = useState<any>()
 
   useEffect(() => {
     const getData = async () => {
+      console.time('get-model-req')
       const res1 = await (await fetch(`/api/getChampionModels/aatrox`)).json()
       const [folder, file] = res1.glbs[0].Key.split('/')
 
-      console.log({ folder, file })
       const res2 = await (await fetch(`/api/getAwsObject/${folder}/${file}`)).json()
 
-      setGlb(res1)
-      setObj(res2)
+      const blob = new Blob([res2.binaryStr], { type: 'application/javascript' })
+      const glb = new File([blob], 'custom-file.glb')
+      const url = URL.createObjectURL(blob)
+
+      try {
+        const module = await import(url)
+        console.log({ module })
+        setBlob(module)
+      } catch (err) {
+        console.error(err)
+      }
+
+      console.log({ folder, file, glb, blob, url })
+      console.timeEnd('get-model-req')
     }
 
     getData()
   }, [])
 
-  console.log(glb)
-  // console.log(obj)
-
-  return null
+  if (!blob) return null
 
   switch (skin) {
     case 'skin0':
-      return <Skin0 glb={glb} timerLabel={`aatrox-${skin}`} />
+      return <Skin0 glb={blob} timerLabel={`aatrox-${skin}`} />
     case 'skin1':
-      return <Skin1 glb={glb} timerLabel={`aatrox-${skin}`} />
+      return <Skin1 glb={blob} timerLabel={`aatrox-${skin}`} />
     case 'skin2':
-      return <Skin2 glb={glb} timerLabel={`aatrox-${skin}`} />
+      return <Skin2 glb={blob} timerLabel={`aatrox-${skin}`} />
     default:
       return null
   }
