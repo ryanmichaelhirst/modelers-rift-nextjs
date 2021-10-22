1eecfc092e393046190bcd041f135cd16d3bfade
RyanMichael <rmbh4211995@gmail.com>
2021-10-22 18:32:50 -0400

    Add mui. Add search input for champion select

--- client/src/components/GridSelect.tsx
+++ client/src/components/GridSelect.tsx
@@ -1,3 +1,4 @@
+import Input from '@components/Input'
 import { chooseChampion } from '@store/slices/championSlice'
 import classNames from 'classnames'
 import { useEffect, useState } from 'react'
@@ -15,32 +16,52 @@ const GridSelect = ({ items }: { items: any[] }) => {
     }
   }, [items])
 
+  const onInput = (e: React.SyntheticEvent<Element, Event>, value: any, reason: string) => {
+    if (reason !== 'selectOption') return
+
+    const item = items.find((i) => i.name === value)
+    if (!item) return
+
+    dispatch(chooseChampion('playerChampion', item))
+    setSelected(item)
+  }
+
   const onClick = (item: Record<string, any>) => () => {
     dispatch(chooseChampion('playerChampion', item))
     setSelected(item)
   }
 
-  return (
-    <div
-      className='grid grid-flow-row grid-cols-3 gap-0.5 overflow-y-auto h-96'
-      style={{ transform: 'perspective(450px) rotateY(15deg)' }}
-    >
-      {items.map((i, idx) => {
-        const active = i.name === selected?.name
+  console.log(selected)
 
-        return (
-          <div
-            key={idx}
-            className={classNames('bg-cover bg-center h-28 text-white', !active && 'opacity-70')}
-            style={{
-              backgroundImage: `url(${i.square_asset})`,
-            }}
-            onClick={onClick(i)}
-          >
-            <p>{i.name}</p>
-          </div>
-        )
-      })}
+  return (
+    <div style={{ transform: 'perspective(450px) rotateY(15deg)' }}>
+      {selected?.name && (
+        <Input
+          onChange={onInput}
+          value={selected.name}
+          classes='mb-4'
+          options={items.map((i) => i.name)}
+          label='Select your champion'
+        />
+      )}
+      <div className='grid grid-flow-row grid-cols-3 gap-0.5 overflow-y-auto h-96'>
+        {items.map((i, idx) => {
+          const active = i.name === selected?.name
+
+          return (
+            <div
+              key={idx}
+              className={classNames('bg-cover bg-center h-28 text-white', !active && 'opacity-70')}
+              style={{
+                backgroundImage: `url(${i.square_asset})`,
+              }}
+              onClick={onClick(i)}
+            >
+              <p>{i.name}</p>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
---
+++ client/src/components/Input.tsx
@@ -0,0 +1,42 @@
+import Autocomplete from '@mui/material/Autocomplete'
+import Stack from '@mui/material/Stack'
+import TextField from '@mui/material/TextField'
+import classNames from 'classnames'
+import React from 'react'
+
+const Input = ({
+  value,
+  options,
+  label,
+  classes = '',
+  onChange,
+}: {
+  options: any[]
+  label: string
+  classes?: string
+  value: string
+  onChange: (e: React.SyntheticEvent<Element, Event>, value: any, reason: string) => void
+}) => {
+  return (
+    <Stack spacing={2}>
+      <Autocomplete
+        onChange={onChange}
+        className={classNames(classes)}
+        options={options}
+        value={value}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label={label}
+            InputProps={{
+              ...params.InputProps,
+              type: 'search',
+            }}
+          />
+        )}
+      />
+    </Stack>
+  )
+}
+
+export default Input
