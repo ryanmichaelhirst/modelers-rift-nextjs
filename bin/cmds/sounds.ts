d72c23882a0fada59eabe448c2be7fa16bcbfe07
RyanMichael <rmbh4211995@gmail.com>
2021-12-24 06:27:31 -0500

    Add pqueue, error handling, and time logging to generate-voice-lines script.

--- bin/cmds/sounds.ts
+++ bin/cmds/sounds.ts
@@ -1,4 +1,4 @@
-import { execSync } from 'child_process'
+import { exec, execSync } from 'child_process'
 import dotenv from 'dotenv'
 import fs from 'fs'
 import PQueue from 'p-queue'
@@ -6,7 +6,7 @@ import path from 'path'
 
 dotenv.config()
 
-const queue = new PQueue({ concurrency: 30 })
+const queue = new PQueue({ concurrency: 300 })
 
 export const generateVoiceLines = async ({
   input,
@@ -17,18 +17,43 @@ export const generateVoiceLines = async ({
   output?: string
   region?: string
 }) => {
+  console.time('generate-voice-lines')
+
   const inputDir = input || path.join(process.env.APP_HOME, 'input')
   const outputDir = output || path.join(process.env.APP_HOME, 'output')
 
+  // create input directory if it doesn't exist
   try {
+    execSync(`mkdir ${inputDir}`)
+  } catch (err) {
+    console.error(`Input dir @ ${inputDir} already exists`)
+  }
+
+  // create output directory if it doesn't exist, or wipe if it does exist
+  try {
+    execSync(`mkdir ${outputDir}`)
+  } catch (err) {
+    try {
+      console.error(`Ouput dir @ ${inputDir} already exists, wiping the directory`)
+      execSync(`rm -r ${outputDir}`)
+      execSync(`mkdir ${outputDir}`)
+    } catch (e) {
+      throw new Error(`Failed to wipe ${outputDir}`)
+    }
+  }
+
+  try {
+    // read extracted champion assets from WAD files from Obsidian
     const champDirPath = path.join(inputDir, `assets/sounds/wwise2016/vo/${region}/characters`)
     const champDirs = fs.readdirSync(champDirPath)
 
     for (const cdir of champDirs) {
+      // find all skin folders for each champion
       const skinDirPath = path.join(champDirPath, cdir, 'skins')
       const skinDirs = fs.readdirSync(skinDirPath)
 
       for (const sdir of skinDirs) {
+        // get all sound files for each skin folder
         const filesPath = path.join(skinDirPath, sdir)
         const files = fs.readdirSync(filesPath)
 
@@ -47,20 +72,28 @@ export const generateVoiceLines = async ({
         const eventPath = path.join(filesPath, `${cdir}_${sdir}_vo_events.bnk`)
         const outputPath = path.join(outputDir, cdir, binFile)
 
-        // console.log({ binPath, audioPath, eventPath })
-
-        if (!cdir.includes('aatrox')) continue
-
+        // extract .ogg files from bnk sound files
         try {
-          execSync(
-            `${execPath} --audio ${audioPath} --bin ${binPath} --events ${eventPath} -o ${outputPath} --oggs-only`,
-          )
+          queue.add(() => {
+            exec(
+              `${execPath} --audio ${audioPath} --bin ${binPath} --events ${eventPath} -o ${outputPath} --oggs-only`,
+            )
+          })
         } catch (err) {
-          throw new Error('Could not run bnk-extract')
+          console.error(`Could not run bnk-extract for ${outputPath}`)
         }
       }
+
+      console.log(`Extracted voice lines for ${cdir}`)
     }
+
+    // wait until queue empties before completing script
+    await queue.onIdle()
+
+    console.log('Finished extracting voice lines')
   } catch (err) {
-    throw new Error('Could not get champion directories')
+    throw new Error('Could not find champion directories')
   }
+
+  console.timeEnd('generate-voice-lines')
 }
