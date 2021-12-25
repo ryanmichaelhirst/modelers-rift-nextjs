de9d795cf01881b9eeb423e52fb26c8e364afe23
RyanMichael <rmbh4211995@gmail.com>
2021-12-25 00:58:12 -0500

    Add renaming logic to voice line files. Rename extractVoiceLines / add generateVoiceLines

--- bin/cmds/sounds.ts
+++ bin/cmds/sounds.ts
@@ -8,7 +8,7 @@ dotenv.config()
 
 const queue = new PQueue({ concurrency: 50 })
 
-export const generateVoiceLines = async ({
+export const extractVoiceLines = async ({
   input,
   output,
   region = 'en_us',
@@ -96,4 +96,88 @@ export const generateVoiceLines = async ({
   }
 
   console.timeEnd('generate-voice-lines')
+}
+
+export const generateVoiceLines = async ({
+  input,
+  output,
+  region,
+}: {
+  input?: string
+  output?: string
+  region?: string
+}) => {
+  // extract voice lines from extracted Obsidian assets
+  // await extractVoiceLines({ input, output, region })
+
+  // get all champion directories
+  const outputDir = output || path.join(process.env.APP_HOME, 'output')
+  const champDirs = fs.readdirSync(outputDir)
+
+  // iterate over each champion directory
+  for (const cdir of champDirs) {
+    const skinDirPath = path.join(outputDir, cdir)
+    const skinDirs = fs.readdirSync(skinDirPath)
+
+    // if (cdir !== 'aatrox' && cdir !== 'ahri') continue
+
+    // iterate over each skin directory
+    for (const sdir of skinDirs) {
+      const voiceLinePath = path.join(skinDirPath, sdir)
+      const voiceLineDirs = fs.readdirSync(voiceLinePath)
+      let errorCount = 0
+
+      // iterate over voice lines
+      // contents can either be files or directories
+      for (const vdir of voiceLineDirs) {
+        const filesPath = path.join(voiceLinePath, vdir)
+
+        try {
+          const files = fs.readdirSync(filesPath)
+          // console.log(files)
+          // rename each voice line file
+          for (let ii = 0; ii < files.length; ii++) {
+            const fileName = files[ii]
+            const formattedName = formatVoiceLineFileName({ cdir, vdir })
+            const newFileName = ii === 0 ? formattedName : `${formattedName}_${ii}`
+
+            console.log({ newFileName })
+
+            // exec(`mv ${fileName} ${outputDir}/generated/${cdir}/${newFileName}.ogg`)
+          }
+        } catch (err) {
+          // console.error(`Could not read ${filesPath}, assuming it is a file`)
+          const newFileName = `unknown_${errorCount}.ogg`
+          // exec(`mv ${filesPath} ${outputDir}/generated/${cdir}/${sdir}/${newFileName}`)
+          // console.log({ newFileName })
+          errorCount++
+        }
+      }
+    }
+  }
+}
+
+const zeroPadSkinNum = (str: string) => {
+  const num = str.replace('skin', '')
+
+  if (num === '0') return 0
+
+  return num.length === 1 ? `0${num}` : num
+}
+
+const formatVoiceLineFileName = ({ cdir, vdir }: { cdir: string; vdir: string }) => {
+  const champName = cdir.charAt(0).toUpperCase() + cdir.substring(1, cdir.length)
+  // converts 'Play_vo_ZyraSkin16_ZyraBasicAttack_cast3D' --> 'basic_attack_cast'
+  // remove 'Play_vo_Zyra', 'SkinXX', and '3D' or '2D' or '_'
+
+  var champRegex = new RegExp(champName, 'g')
+
+  return vdir
+    .replace(`Play_vo_`, '')
+    .replace(champRegex, '')
+    .replace(/Skin\d+/, '')
+    .replace(/3D|2D|_/g, '')
+    .replace(/[A-Z]/g, (letter, index) =>
+      index === 0 ? letter.toLowerCase() : '_' + letter.toLowerCase(),
+    )
 }
