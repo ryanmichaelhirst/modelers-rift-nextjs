1e838d301f9e13ea9cdaddba4e22b4ec284b5d97
RyanMichael <rmbh4211995@gmail.com>
2021-12-30 04:20:34 -0500

    Add better handling for nested voice line directories

--- bin/cmds/sounds.ts
+++ bin/cmds/sounds.ts
@@ -17,26 +17,20 @@ export const extractVoiceLines = async ({
   output?: string
   region?: string
 }) => {
-  console.time('generate-voice-lines')
+  console.time('extract-voice-lines')
 
   const inputDir = input || path.join(process.env.APP_HOME, 'input')
-  const outputDir = output || path.join(process.env.APP_HOME, 'output')
-
-  // create input directory if it doesn't exist
-  try {
-    execSync(`mkdir ${inputDir}`)
-  } catch (err) {
-    console.error(`Input dir @ ${inputDir} already exists`)
-  }
+  const outputDir = output || path.join(process.env.APP_HOME, 'output/extracted')
 
   // create output directory if it doesn't exist, or wipe if it does exist
+  // mkdir -p --> creates parent directories
   try {
-    execSync(`mkdir ${outputDir}`)
+    execSync(`mkdir -p ${outputDir}`)
   } catch (err) {
     try {
-      console.error(`Ouput dir @ ${inputDir} already exists, wiping the directory`)
+      console.error(`Ouput dir @ ${outputDir} already exists, wiping the directory`)
       execSync(`rm -r ${outputDir}`)
-      execSync(`mkdir ${outputDir}`)
+      execSync(`mkdir -p ${outputDir}`)
     } catch (e) {
       throw new Error(`Failed to wipe ${outputDir}`)
     }
@@ -48,6 +42,8 @@ export const extractVoiceLines = async ({
     const champDirs = fs.readdirSync(champDirPath)
 
     for (const cdir of champDirs) {
+      if (cdir !== 'aatrox' && cdir !== 'ahri') continue
+
       queue.add(() => {
         // find all skin folders for each champion
         const skinDirPath = path.join(champDirPath, cdir, 'skins')
@@ -95,7 +91,7 @@ export const extractVoiceLines = async ({
     throw new Error('Could not find champion directories')
   }
 
-  console.timeEnd('generate-voice-lines')
+  console.timeEnd('extract-voice-lines')
 }
 
 export const generateVoiceLines = async ({
@@ -108,18 +104,35 @@ export const generateVoiceLines = async ({
   region?: string
 }) => {
   // extract voice lines from extracted Obsidian assets
-  // await extractVoiceLines({ input, output, region })
+  await extractVoiceLines({ input, output, region })
+
+  console.time('generate-voice-lines')
 
   // get all champion directories
-  const outputDir = output || path.join(process.env.APP_HOME, 'output')
-  const champDirs = fs.readdirSync(outputDir)
+  const inputDir = output || path.join(process.env.APP_HOME, 'output/extracted')
+  const outputDir = path.join(process.env.APP_HOME, 'output/generated')
+  const champDirs = fs.readdirSync(inputDir)
+
+  // create output directory if it doesn't exist, or wipe if it does exist
+  // mkdir -p --> creates parent directories
+  try {
+    execSync(`mkdir -p ${outputDir}`)
+  } catch (err) {
+    try {
+      console.error(`Ouput dir @ ${outputDir} already exists, wiping the directory`)
+      execSync(`rm -r ${outputDir}`)
+      execSync(`mkdir -p ${outputDir}`)
+    } catch (e) {
+      throw new Error(`Failed to wipe ${outputDir}`)
+    }
+  }
 
   // iterate over each champion directory
   for (const cdir of champDirs) {
-    const skinDirPath = path.join(outputDir, cdir)
+    const skinDirPath = path.join(inputDir, cdir)
     const skinDirs = fs.readdirSync(skinDirPath)
 
-    // if (cdir !== 'aatrox' && cdir !== 'ahri') continue
+    if (cdir !== 'aatrox' && cdir !== 'ahri') continue
 
     // iterate over each skin directory
     for (const sdir of skinDirs) {
@@ -140,29 +153,30 @@ export const generateVoiceLines = async ({
             const fileName = files[ii]
             const formattedName = formatVoiceLineFileName({ cdir, vdir })
             const newFileName = ii === 0 ? formattedName : `${formattedName}_${ii}`
-
-            console.log({ newFileName })
-
-            // exec(`mv ${fileName} ${outputDir}/generated/${cdir}/${newFileName}.ogg`)
+            const newFilePath = `${outputDir}/${cdir}/${sdir}/${newFileName}.ogg`
+            console.log(`cp ${filesPath}/${fileName} ${newFilePath}`)
+            execSync(`mkdir -p output/generated/${cdir}/${sdir}`)
+            execSync(`cp ${filesPath}/${fileName} ${newFilePath}`)
           }
         } catch (err) {
-          // console.error(`Could not read ${filesPath}, assuming it is a file`)
-          const newFileName = `unknown_${errorCount}.ogg`
-          // exec(`mv ${filesPath} ${outputDir}/generated/${cdir}/${sdir}/${newFileName}`)
-          // console.log({ newFileName })
-          errorCount++
+          console.error(`Could not read ${filesPath}, determing if it is a file`)
+          const fileStat = fs.lstatSync(filesPath)
+          const isFile = fileStat.isFile()
+
+          if (isFile) {
+            const newFileName = `unknown_${errorCount}`
+            const newFilePath = `${outputDir}/${cdir}/${sdir}/${newFileName}.ogg`
+            console.log(`cp ${filesPath} ${newFilePath}`)
+            execSync(`mkdir -p output/generated/${cdir}/${sdir}`)
+            execSync(`cp ${filesPath} ${newFilePath}`)
+            errorCount++
+          }
         }
       }
     }
   }
-}
 
-const zeroPadSkinNum = (str: string) => {
-  const num = str.replace('skin', '')
-
-  if (num === '0') return 0
-
-  return num.length === 1 ? `0${num}` : num
+  console.timeEnd('generate-voice-lines')
 }
 
 const formatVoiceLineFileName = ({ cdir, vdir }: { cdir: string; vdir: string }) => {
