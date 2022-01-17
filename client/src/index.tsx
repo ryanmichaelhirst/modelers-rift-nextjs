a55020ca1ad4d8245a9495aa2a73640794cd11ba
RyanMichael <rmbh4211995@gmail.com>
2022-01-17 14:59:32 -0500

    Add apollo client to app

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,3 +1,4 @@
+import { ApolloProvider } from '@apollo/client'
 import { App } from '@components/App'
 import { Theme } from '@components/Theme'
 import { store } from '@store/index'
@@ -7,16 +8,21 @@ import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import '../styles/index.css'
 import '../styles/tailwind.css'
+import { apolloClient } from './api/index'
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <SnackbarProvider maxSnack={3}>
-        <Theme>
-          <App />
-        </Theme>
-      </SnackbarProvider>
-    </BrowserRouter>
-  </Provider>,
+  <ApolloProvider client={apolloClient}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <SnackbarProvider maxSnack={3}>
+          <Theme>
+            <App />
+          </Theme>
+        </SnackbarProvider>
+      </BrowserRouter>
+    </Provider>
+    ,
+  </ApolloProvider>,
+
   document.getElementById('app'),
 )
