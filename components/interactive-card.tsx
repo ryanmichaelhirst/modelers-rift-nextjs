61d6aff49c5fd0f4fad7ce9a594292dd218b4035
RyanMichael <rmbh4211995@gmail.com>
2022-03-12 20:55:13 -0500

    Add better flex layout for card

--- components/interactive-card.tsx
+++ components/interactive-card.tsx
@@ -18,7 +18,7 @@ const InteractiveCard = () => {
 
   const sfx = data?.character?.assets?.filter((a) => a?.type === 'sfx')
 
-  console.log(data, sfx)
+  console.log({ data, sfx, error })
 
   return (
     <GlassCard classes='flex flex-col h-full' hasPadding={false}>
@@ -31,31 +31,41 @@ const InteractiveCard = () => {
         </p>
         <SkinCarousel />
       </div>
-      <div className='flex-auto'>
-        <GlassCard classes='bg-white/70 rounded-t-lg p-4' hasPadding={false} rounded={false}>
-          <div style={{ color: '#FF6BD0', backgroundColor: '$#FECFEF' }}>Audios</div>
-          <table className='h-5/6 overflow-auto'>
-            <thead>
-              <tr>
-                <td>#</td>
-                <td>Title</td>
-                <td>Type</td>
-                <td>Time</td>
-              </tr>
-            </thead>
-            <tbody>
-              {sfx?.slice(0, 100).map((s, idx) => (
-                <tr key={s?.path}>
-                  <td>{idx}</td>
-                  <td>{s?.name}</td>
-                  <td>{s?.type}</td>
-                  <td>{s?.duration}</td>
+      <div className='flex-auto flex flex-col'>
+        <GlassCard
+          classes='flex-auto flex flex-col bg-white/70 rounded-t-lg p-4'
+          hasPadding={false}
+          rounded={false}
+        >
+          <div className='flex-initial'>
+            <div className='text-[#FF6BD0] bg-[#FECFEF] inline-block px-3 py-1 rounded-lg'>
+              Audios
+            </div>
+          </div>
+          <div className='mt-4 flex-auto overflow-y-auto max-h-60'>
+            <table className='w-full'>
+              <thead>
+                <tr>
+                  <td>#</td>
+                  <td>Title</td>
+                  <td>Type</td>
+                  <td>Time</td>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {sfx?.slice(0, 20).map((s, idx) => (
+                  <tr key={s?.path}>
+                    <td>{idx}</td>
+                    <td>{s?.name}</td>
+                    <td>{s?.type}</td>
+                    <td>{s?.duration}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </GlassCard>
-        <div className='rounded-b-lg bg-white h-1/6 flex items-center justify-start'>
+        <div className='flex flex-[0_0_50px] items-center justify-start rounded-b-lg bg-white'>
           <div>
             <IconButton>
               <HeadphonesOutlined />
