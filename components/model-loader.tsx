24bf0fd8370434a2e0ca0391554fdbe436e7d8a1
RyanMichael <rmbh4211995@gmail.com>
2022-04-05 23:19:39 -0400

    Lint

--- components/model-loader.tsx
+++ components/model-loader.tsx
@@ -2,5 +2,6 @@ import { Html, useProgress } from '@react-three/drei'
 
 export const ModelLoader = () => {
   const { progress } = useProgress()
+
   return <Html center>{progress}% loaded</Html>
 }
