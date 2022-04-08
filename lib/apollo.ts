698569c25c1702c4ea70d04bd1f9ca3f79964ad7
RyanMichael <rmbh4211995@gmail.com>
2022-04-08 17:28:03 -0400

    Fix graphql uri env var. Remove nodemon

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 export const apolloClient = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_GRAPHQL_URI + '/api/graphql',
+  uri: process.env.NEXT_PUBLIC_GRAPHQL_URI,
   cache: new InMemoryCache(),
 })
