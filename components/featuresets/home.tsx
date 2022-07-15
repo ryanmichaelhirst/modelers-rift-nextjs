d187b52bc4a68a85efb418c9d9b8310987657339
RyanMichael Hirst <rmbh4211995@gmail.com>
2022-07-15 18:13:26 -0400

    Add 'not enough' section to homepage

--- components/featuresets/home.tsx
+++ components/featuresets/home.tsx
@@ -2,7 +2,7 @@ import { AssetCard } from '@components/asset-card'
 import { Button } from '@components/button'
 import { Carousel } from '@components/carousel'
 import { useCharactersQuery } from '@graphql/generated/types'
-import { LinearProgress } from '@mui/material'
+import { KeyboardArrowDown } from '@mui/icons-material'
 import { useRouter } from 'next/router'
 import { FC } from 'react'
 
@@ -10,41 +10,45 @@ export const Home: FC<ReturnType<typeof useCharactersQuery>> = ({ data, loading,
   const router = useRouter()
 
   return (
-    <div className='mt-10'>
-      <div className='flex-col space-x-20'>
-        <div className='w-[600px]'>
-          <p className='text-primary text-4xl font-nunito mb-8'>
-            Bringing the champions you love to The Web
-          </p>
-          <p className='text-tertiary text-2xl font-nunito mb-8'>
-            Explore every champion, skin, voice line, sound effect, and animation in League of
-            Legends. Perfect for viewing new releases and store content.
-          </p>
-          <Button
-            classes={{
-              root: 'font-nunito bg-primary py-3 px-7 text-white',
-            }}
-            text='Show me models'
-            onClick={() => router.push('models')}
+    <>
+      <div className='mt-10'>
+        <div className='flex space-x-20'>
+          <div>
+            <p className='text-primary text-4xl font-nunito mb-8'>
+              Bringing the champions you love to The Web
+            </p>
+            <p className='text-tertiary text-2xl font-nunito mb-8'>
+              Explore every champion, skin, voice line, sound effect, and animation in League of
+              Legends. Perfect for viewing new releases and store content.
+            </p>
+            <Button
+              classes={{
+                root: 'font-nunito bg-primary py-3 px-7 text-white',
+              }}
+              text='Show me models'
+              onClick={() => router.push('models')}
+            />
+          </div>
+          <Carousel
+            items={
+              data?.characters?.collection?.map((c) => {
+                const asset = c?.assets?.find((a) => a?.type === 'model')
+                // TODO: fix character type?
+                // @ts-ignore
+
+                return <AssetCard key={c?.id} character={c} asset={asset} />
+              }) ?? []
+            }
           />
         </div>
-        {loading && (
-          <div className='grow'>
-            <LinearProgress />
-          </div>
-        )}
-        <Carousel
-          items={
-            data?.characters?.collection?.map((c) => {
-              const asset = c?.assets?.find((a) => a?.type === 'model')
-              // TODO: fix character type?
-              // @ts-ignore
-
-              return <AssetCard key={c?.id} character={c} asset={asset} />
-            }) ?? []
-          }
-        />
       </div>
-    </div>
+      <div className='flex flex-col items-center justify-center font-nunito'>
+        <p className='text-primary text-4xl mb-4'>Not enough?</p>
+        <p className='text-tertiary text-xl font-nunito mb-4'>
+          Take a look at what else we have to offer
+        </p>
+        <KeyboardArrowDown className='text-primary text-4xl' />
+      </div>
+    </>
   )
 }
--- components/model-loader.tsx
+++ components/model-loader.tsx
@@ -1,7 +1,13 @@
+import { CircularProgress } from '@mui/material'
 import { Html, useProgress } from '@react-three/drei'
 
 export const ModelLoader = () => {
   const { progress } = useProgress()
+  console.log(progress)
 
-  return <Html center>{progress}% loaded</Html>
+  return (
+    <Html center>
+      <CircularProgress variant='determinate' value={progress} />
+    </Html>
+  )
 }
