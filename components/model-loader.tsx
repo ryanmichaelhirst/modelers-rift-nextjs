b48f413b43c37644720753448641618a9e68f054
RyanMichael <rmbh4211995@gmail.com>
2022-07-23 18:15:06 -0400

    Remove console.log from ModelLoader

--- components/model-loader.tsx
+++ components/model-loader.tsx
@@ -3,7 +3,6 @@ import { Html, useProgress } from '@react-three/drei'
 
 export const ModelLoader = () => {
   const { progress } = useProgress()
-  console.log(progress)
 
   return (
     <Html center>
