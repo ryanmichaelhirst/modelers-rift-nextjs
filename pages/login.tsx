c7d152eeb3babc173c2b6538ea10b389d569c38c
RyanMichael B Hirst <ryanmichaelbhirst@macbook-pro.myfiosgateway.com>
2022-08-07 14:32:04 -0400

    Login page css

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,90 @@
+import { Button } from '@components/button'
 import type { NextPage } from 'next'
+import Link from 'next/link'
+import { useForm } from 'react-hook-form'
 
 const Login: NextPage = () => {
-  return <div>Login page</div>
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors, isSubmitted, isSubmitSuccessful },
+    setValue,
+    getValues,
+    setError,
+  } = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  })
+
+  const onSubmit = handleSubmit(async (data) => {
+    if (!data.email.includes('@')) {
+      setError('email', { message: "Email address must include '@'" }, { shouldFocus: true })
+
+      return
+    }
+
+    if (!data.email.includes('.')) {
+      setError('email', { message: 'Invalid email address' }, { shouldFocus: true })
+
+      return
+    }
+
+    setValue('email', '')
+    setValue('password', '')
+  })
+
+  return (
+    <div className='flex items-center justify-center h-full'>
+      <div className='flex flex-col w-[400px] p-10 border border-solid border-slate-200 rounded-lg shadow-lg'>
+        <h1 className='text-tertiary text-3xl'>Welcome back</h1>
+
+        <div className='flex mt-4 mb-10'>
+          <span className='mr-2'>Don't have an account?</span>
+          <Link href='/sign-up'>
+            <a className='text-primary underline hover:opacity-80'>Sign up</a>
+          </Link>
+        </div>
+
+        <div className='flex flex-col mb-4'>
+          <label className='mb-1 text-tertiary'>Email</label>
+          <input
+            className='border border-solid border-slate-300 rounded text-slate-300 px-2'
+            placeholder='Email'
+            value={watch('email')}
+            {...register('email', { required: 'Email is required' })}
+          />
+          <span className='ml-2 text-red-600 mt-2'>
+            {errors.email && <p>{errors.email.message}</p>}
+          </span>
+        </div>
+
+        <div className='flex flex-col mb-4'>
+          <label className='mb-1 text-tertiary'>Password</label>
+          <input
+            className='border border-solid border-slate-300 rounded text-slate-300 px-2'
+            placeholder='Password'
+            value={watch('password')}
+            {...register('password', { required: 'Password is required' })}
+          />
+          <span className='ml-2 text-red-600 mt-2'>
+            {errors.password && <p>{errors.password.message}</p>}
+          </span>
+        </div>
+
+        <Button
+          id='login'
+          onClick={onSubmit}
+          classes={{
+            button: 'mt-4 !py-2 !px-5 text-primary text-lg',
+          }}
+          text={'Login'}
+        />
+      </div>
+    </div>
+  )
 }
 
 export default Login
--- pages/sign-up.tsx
+++ pages/sign-up.tsx
@@ -40,23 +40,7 @@ const SignUp: NextPage = () => {
   return (
     <div className='flex items-center justify-center h-full'>
       <div className='flex flex-col w-[400px] p-10 border border-solid border-slate-200 rounded-lg shadow-lg'>
-        <div>
-          <p>With an account you can:</p>
-          <ul className='mt-2'>
-            <li className='flex items-center pl-2'>
-              <DocumentDownloadIcon className='h-5 w-5 text-primary' />
-              <span className='ml-2'>Download 1 free model</span>
-            </li>
-            <li className='flex items-center pl-2'>
-              <StarIcon className='h-5 w-5 text-primary' />
-              <span className='ml-2'>Create a favorites list</span>
-            </li>
-            <li className='flex items-center pl-2'>
-              <AnnotationIcon className='h-5 w-5 text-primary' />
-              <span className='ml-2'>Make posts in the community</span>
-            </li>
-          </ul>
-        </div>
+        <h1 className='text-tertiary text-3xl'>Create an account</h1>
 
         <div className='flex mt-4 mb-10'>
           <span className='mr-2'>Already have an account?</span>
@@ -89,6 +73,24 @@ const SignUp: NextPage = () => {
           <span className='ml-2 text-red-600 mt-2'>
             {errors.password && <p>{errors.password.message}</p>}
           </span>
+        </div>
+
+        <div className='mt-2 mb-6'>
+          <p className='text-lg'>The following comes with your free account!</p>
+          <ul className='mt-2'>
+            <li className='flex items-center pl-2 my-2'>
+              <DocumentDownloadIcon className='h-5 w-5 text-primary' />
+              <span className='ml-2'>Download 1 free model</span>
+            </li>
+            <li className='flex items-center pl-2 my-2'>
+              <StarIcon className='h-5 w-5 text-primary' />
+              <span className='ml-2'>Create a favorites list</span>
+            </li>
+            <li className='flex items-center pl-2 my-2'>
+              <AnnotationIcon className='h-5 w-5 text-primary' />
+              <span className='ml-2'>Access to community</span>
+            </li>
+          </ul>
         </div>
 
         <Button
