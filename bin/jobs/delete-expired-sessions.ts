5200613db89a1f6a454ed07b3281043537882e31
RyanMichael B Hirst <ryanmichaelbhirst@ryanmichaelsmbp.lan>
2022-08-16 22:43:48 -0400

    Add redis delete-expired-sesions script

---
+++ bin/jobs/delete-expired-sessions.ts
@@ -0,0 +1,27 @@
+import { redisService, Session } from '@utils/server-helpers'
+import { logger } from 'logger'
+
+/**
+ * Average Runtime: 2.542s
+ */
+export default async () => {
+  const redisKeys = await redisService.client().keys('*')
+  // ISO 8601 standard
+  // https://stackoverflow.com/questions/30209941/what-t-and-z-means-in-date#:~:text=The%20ISO%208601%20standard%20defines,per%20military%20and%20aviation%20tradition.
+  const now = new Date()
+
+  for (const key of redisKeys) {
+    const value = (await redisService.client().json.get(key)) as Session
+    const expiredAt = new Date(value.expiredAt)
+
+    if (now >= expiredAt) {
+      // session has expired, delete from redis
+      await redisService.client().json.del(key)
+      logger.info({ expired: true, expiredAt, now, key })
+    } else {
+      logger.info({ expired: false, expiredAt, now, key })
+    }
+  }
+
+  redisService.destroy()
+}
