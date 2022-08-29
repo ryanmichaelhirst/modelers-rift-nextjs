313697fcb0f8405324244ac2e2d27406a41e1129
RyanMichael Hirst <rmbh4211995@gmail.com>
2022-08-29 17:29:42 -0400

    Add model-explorer screenshot to homepage

--- components/featuresets/home.tsx
+++ components/featuresets/home.tsx
@@ -13,6 +13,7 @@ import {
   ExternalLinkIcon,
   VolumeUpIcon,
 } from '@heroicons/react/outline'
+import Image from 'next/image'
 import { useRouter } from 'next/router'
 import { FC } from 'react'
 
@@ -29,7 +30,7 @@ export const Home: FC<CharactersQueryHookResult> = ({ data, loading, error }) =>
     <>
       <div className='flex flex-col justify-between h-full'>
         <div className='flex flex-col mt-4 md:flex-row md:space-x-20 md:mt-10'>
-          <div className='w-3/5'>
+          <div className='w-full md:w-3/5'>
             <p className='text-primary text-xl mb-2 md:text-4xl md:mb-8'>
               Bringing the champions you love to the web
             </p>
@@ -39,7 +40,7 @@ export const Home: FC<CharactersQueryHookResult> = ({ data, loading, error }) =>
             </p>
             <Button text='Show me models' onClick={() => router.push('models')} />
           </div>
-          <div className='w-2/5'>
+          <div className='mt-10 w-[300px] text-center self-center md:text-left md:mt-0 md:w-2/5'>
             <Card>
               <p className='text-tertiary font-nunito font-bold text-lg capitalize'>Aatrox</p>
               <p>The Darkin Blade</p>
@@ -57,7 +58,7 @@ export const Home: FC<CharactersQueryHookResult> = ({ data, loading, error }) =>
             </Card>
           </div>
         </div>
-        <div className='flex flex-col items-center justify-center mb-4'>
+        <div className='flex flex-col items-center justify-center mb-4 mt-10 md:mt-0'>
           <p className='text-primary text-xl md:text-4xl md:mb-4'>Not enough?</p>
           <p className='text-tertiary text-lg md:text-xl md:mb-4'>
             Take a look at what else we have to offer
@@ -71,16 +72,28 @@ export const Home: FC<CharactersQueryHookResult> = ({ data, loading, error }) =>
         </div>
       </div>
 
-      <div id='app-overview' className='w-1/2 pt-32 mb-32'>
-        <p className='text-primary text-lg mb-4 font-bold'>Designed for league fanatics</p>
-        <p className='text-secondary text-xl font-bold mb-4'>
-          Easily find your favorite assets and save them for later
-        </p>
-        <p className='text-tertiary'>
-          After being out for over a decade, players still don't have an accessible solution to
-          viewing in game assets. We change that today by bringing them to you in our polished model
-          explorer.
-        </p>
+      <div className='flex flex-col items-center justify-center md:flex-row'>
+        <div id='app-overview' className='w-1/2 pt-32 mb-32 md:mr-[150px]'>
+          <p className='text-primary text-lg mb-4 font-bold'>Designed for league fanatics</p>
+          <p className='text-secondary text-xl font-bold mb-4'>
+            Easily find your favorite assets and save them for later
+          </p>
+          <p className='text-tertiary'>
+            After being out for over a decade, players still don't have an accessible solution to
+            viewing in game assets. We change that today by bringing them to you in our polished
+            model explorer.
+          </p>
+        </div>
+
+        <div className='flex items-center justify-center relative w-full mb-32 h-[240px] md:mb-0 md:w-1/2'>
+          <Image
+            layout='fill'
+            objectFit='cover'
+            src={'/explorer-screenshot.png'}
+            className='rounded'
+            alt={'model explorer table'}
+          />
+        </div>
       </div>
 
       <div className='mb-32'>
