ee2d673fe53fd3f19b51c27f564f93019efc8ffb
rmbh4211995 <rmbh4211995@gmail.com>
2022-09-02 00:57:21 -0400

    Remove browser s3 env vars

--- lib/s3.ts
+++ lib/s3.ts
@@ -2,10 +2,8 @@ import { S3Client } from '@aws-sdk/client-s3'
 
 export const BUCKET_NAME = 'league-of-legends-assets'
 
-const accessKeyId =
-  process.env.S3_AWS_ACCESS_KEY_ID ?? process.env.NEXT_PUBLIC_S3_AWS_ACCESS_KEY_ID ?? ''
-const secretAccessKey =
-  process.env.S3_AWS_SECRET_ACCESS_KEY ?? process.env.NEXT_PUBLIC_S3_AWS_SECRET_ACCESS_KEY ?? ''
+const accessKeyId = process.env.S3_AWS_ACCESS_KEY_ID ?? ''
+const secretAccessKey = process.env.S3_AWS_SECRET_ACCESS_KEY ?? ''
 
 export const s3 = new S3Client({
   region: 'us-east-1',
