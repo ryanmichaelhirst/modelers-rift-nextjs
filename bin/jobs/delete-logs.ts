b3c352301bc0bcd13c1c46d5c466ec362d38c72a
rmbh4211995 <rmbh4211995@gmail.com>
2022-09-26 21:13:13 -0400

    Add delete-logs job

---
+++ bin/jobs/delete-logs.ts
@@ -0,0 +1,21 @@
+import { logger } from '@lib/logger'
+import fs from 'fs'
+import path from 'path'
+
+export const deleteLogs = async () => {
+  const logs = await (
+    await fs.promises.readdir('logs')
+  ).filter((filename) => filename !== '.gitignore')
+
+  for (const log of logs) {
+    try {
+      const file = path.join('logs', log)
+      await fs.promises.rm(file)
+      logger.info(`removed log @ ${file}`)
+    } catch (err) {
+      logger.info(`Failed to delete ${log}`)
+    }
+  }
+}
+
+export default deleteLogs
