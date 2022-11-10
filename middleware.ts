ac761ffc59c99db3c2c796132c17aff356b31b09
rmbh4211995 <rmbh4211995@gmail.com>
2022-11-10 15:37:27 -0500

    Add redirect on donation page if user is not logged in

---
+++ middleware.ts
@@ -0,0 +1,18 @@
+// https://nextjs.org/docs/advanced-features/middleware#using-cookies
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+// must set cookies in middleware to access in getServerSideProps
+export function middleware(request: NextRequest) {
+  // Assume a "Cookie:nextjs=fast" header to be present on the incoming request
+  // Getting cookies from the request using the `RequestCookies` API
+  const token = request.cookies.get('token')
+  const refresh = request.cookies.get('refresh')
+
+  // Setting cookies on the response using the `ResponseCookies` API
+  const response = NextResponse.next()
+  if (token) response.cookies.set('token', token)
+  if (refresh) response.cookies.set('refresh', refresh)
+
+  return response
+}
