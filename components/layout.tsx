010d9c4a49f3f9cfbe712e2b694b74b68dee0cec
rmbh4211995 <rmbh4211995@gmail.com>
2022-11-10 23:30:49 -0500

    Add footer and required league statement

--- components/layout.tsx
+++ components/layout.tsx
@@ -19,7 +19,16 @@ export const Layout: FC<PropsWithChildren> = ({ children }) => {
       <header className='sticky top-0 z-20 h-[10vh] bg-white'>
         <MenuBar />
       </header>
-      <div className='mx-4 my-6 h-[90vh] md:mx-16'>{children}</div>
+      <div className='mx-4 my-6 min-h-[90vh] md:mx-16'>{children}</div>
+      <footer className='mx-4 mb-10 text-xs text-slate-300 md:mx-16'>
+        <hr className='mb-4' />
+        <p>
+          © 2022 Modeler's Rift. Modeler's Rift isn’t endorsed by Riot Games and doesn’t reflect the
+          views or opinions of Riot Games or anyone officially involved in producing or managing
+          League of Legends. League of Legends and Riot Games are trademarks or registered
+          trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.
+        </p>
+      </footer>
     </div>
   )
 }
