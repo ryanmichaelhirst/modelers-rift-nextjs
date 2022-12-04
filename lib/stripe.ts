24af8063720ac8852d31c3fd2ee8568a9470e092
rmbh4211995 <rmbh4211995@gmail.com>
2022-12-04 12:32:31 -0500

    Upgrade stripe to latest api version (2022-11-15)

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -2,4 +2,4 @@ import Stripe from 'stripe'
 
 const stripeApiKey = process.env.STRIPE_API_KEY ?? ''
 
-export const stripe = new Stripe(stripeApiKey, { apiVersion: '2022-08-01' })
+export const stripe = new Stripe(stripeApiKey, { apiVersion: '2022-11-15' })
--- pages/api/stripe/event.ts
+++ pages/api/stripe/event.ts
@@ -48,6 +48,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         },
       })
     }
+
     res.status(200).send('OK')
   } catch (err) {
     res.status(500).send('Error parsing stripe event')
