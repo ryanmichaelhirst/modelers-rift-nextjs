3d1eaaf62fe6141b246544595211b24b436ea1b3
rmbh4211995 <rmbh4211995@gmail.com>
2022-12-18 20:48:15 -0500

    Clean up patreon webhook endpoint

--- pages/api/patreon/index.ts
+++ pages/api/patreon/index.ts
@@ -17,16 +17,19 @@ export const config = {
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  patreonLogger.info('Hit patreon endpoint')
+  if (req.method !== 'POST') {
+    patreonLogger.info('Method is not POST', { metadata: { method: req.method } })
+    res.setHeader('Allow', 'POST')
+    res.status(405).end('Method not allowed')
+  }
 
   if (!PATREON_WEBHOOK_SECRET) {
     patreonLogger.error('No patreon webhook secret')
-    res.status(405).send({ error: `No patreon webhook secret` })
+    res.status(405).send('No patreon webhook secret')
 
     return
   }
 
-  const { method } = req
   const patreonEventHeader = req.headers['x-patreon-event']
   const patreonSigHeader = req.headers['x-patreon-signature']
 
@@ -34,23 +37,17 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const hex = crypto.createHmac('md5', PATREON_WEBHOOK_SECRET).update(buf).digest('hex')
   const body: PatreonEvent = await json(req)
 
-  patreonLogger.info(`x-patreon-event header`, { metadata: { patreonEventHeader } })
-  patreonLogger.info(`x-patreon-signature header`, { metadata: { patreonSigHeader } })
+  patreonLogger.info(`patreon request headers`, {
+    metadata: { patreonEventHeader, patreonSigHeader },
+  })
 
-  patreonLogger.info(`Patreon body`, {
+  patreonLogger.info(`Patreon json body`, {
     metadata: { body: util.inspect(body, { showHidden: false, depth: null, colors: true }) },
   })
 
   if (patreonSigHeader !== hex) {
     patreonLogger.error('sig not equal', { metadata: { patreonSigHeader, hex } })
-    res.status(400).send({ error: 'Request signature is unauthorized' })
-
-    return
-  }
-
-  if (method !== 'POST') {
-    patreonLogger.error('method not post')
-    res.status(405).send({ error: `Method ${method} not allowed` })
+    res.status(400).send('Request signature is unauthorized')
 
     return
   }
@@ -78,7 +75,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       break
     case 'members:pledge:delete':
       if (patronEmail === null) {
-        res.status(400).end('No patron email - unable to delete pledge')
+        res.status(400).send('No patron email - unable to delete pledge')
 
         return
       }
