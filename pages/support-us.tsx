429320e935a3ab7a9b1aa227c2ec0a22065234fd
rmbh4211995 <rmbh4211995@gmail.com>
2023-01-04 17:05:49 -0500

    Use s3 images for patreon button graphic

--- pages/support-us.tsx
+++ pages/support-us.tsx
@@ -1,11 +1,22 @@
-import PatreonButton from '@/assets/patreon-button.webp'
+import { awsS3Service } from '@/bin/services/aws-s3-service'
 import { Button } from '@/components/button'
 import { Card } from '@/components/card'
 import { H1 } from '@/components/h1'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
+import { getAwsSignedUrl } from '@/pages/api/aws/signedUrl'
 
-export default () => {
+export const getServerSideProps = async () => {
+  const resp = await getAwsSignedUrl({ key: 'images/patreon-button.webp', expiresIn: 3600 })
+
+  return {
+    props: {
+      patreonButton: resp,
+    },
+  }
+}
+
+export default ({ patreonButton }: { patreonButton?: string }) => {
   const router = useRouter()
 
   return (
@@ -46,7 +57,7 @@ export default () => {
           <div className='relative mx-4 h-[60px] w-[200px] cursor-pointer hover:opacity-50'>
             <Image
               layout='fill'
-              src={PatreonButton}
+              src={patreonButton ?? ''}
               objectFit='contain'
               onClick={() => window.open('https://www.patreon.com/ryanmichaelhirst', '_blank')}
             />
